8167b8b5bcf512f2d1617ec09f4aabf312760e73
Alexander Eide <atlexeide@gmail.com>
2024-10-29 18:27:44 +0100

    .

---
+++ index.js
@@ -0,0 +1,128 @@
+const tempData = [
+  {
+    title: "Work",
+    timeframes: {
+      daily: {
+        current: 5,
+        previous: 7,
+      },
+      weekly: {
+        current: 32,
+        previous: 36,
+      },
+      monthly: {
+        current: 103,
+        previous: 128,
+      },
+    },
+  },
+  {
+    title: "Play",
+    timeframes: {
+      daily: {
+        current: 1,
+        previous: 2,
+      },
+      weekly: {
+        current: 10,
+        previous: 8,
+      },
+      monthly: {
+        current: 23,
+        previous: 29,
+      },
+    },
+  },
+  {
+    title: "Study",
+    timeframes: {
+      daily: {
+        current: 0,
+        previous: 1,
+      },
+      weekly: {
+        current: 4,
+        previous: 7,
+      },
+      monthly: {
+        current: 13,
+        previous: 19,
+      },
+    },
+  },
+  {
+    title: "Exercise",
+    timeframes: {
+      daily: {
+        current: 1,
+        previous: 1,
+      },
+      weekly: {
+        current: 4,
+        previous: 5,
+      },
+      monthly: {
+        current: 11,
+        previous: 18,
+      },
+    },
+  },
+  {
+    title: "Social",
+    timeframes: {
+      daily: {
+        current: 1,
+        previous: 3,
+      },
+      weekly: {
+        current: 5,
+        previous: 10,
+      },
+      monthly: {
+        current: 21,
+        previous: 23,
+      },
+    },
+  },
+  {
+    title: "Self Care",
+    timeframes: {
+      daily: {
+        current: 0,
+        previous: 1,
+      },
+      weekly: {
+        current: 2,
+        previous: 2,
+      },
+      monthly: {
+        current: 7,
+        previous: 11,
+      },
+    },
+  },
+];
+const data = tempData;
+const body = document.body;
+data.forEach((area) => {
+  const card = document.createElement("section");
+  card.classList.add("card");
+
+  const activity = document.createElement("h3");
+  activity.classList.add("activity");
+  activity.textContent = area.title;
+  card.appendChild(activity);
+
+  const nav = document.createElement("nav");
+  nav.classList.add("select");
+  const navButton = document.createElement("button");
+  navButton.classList.add("navButton");
+  const icon = document.createElement("img");
+  icon.src = "./images/icon-ellipsis.svg";
+  icon.alt = "ellipsis icon";
+  navButton.appendChild(icon);
+  nav.appendChild(navButton);
+  card.appendChild(nav);
+
+  body.appendChild(card);
+});
